4ec98c447ffc8bc433ea14c0bcfe0448e643d7af
Thinh Le <timothyle85@gmail.com>
2017-01-31 14:33:59 -0800

    Create navbar.php include file

---
+++ tbpspring2017/navbar.php
@@ -0,0 +1,80 @@
+<?php
+echo '<!-- Top Menu -->
+                <div id="hornav" class="row text-light">
+                    <div class="col-md-21">
+                        <div class="text-center visible-lg">
+                            <ul id="hornavmenu" class="nav navbar-nav">
+                                <li>
+                                    <a href="index.html" class="fa-home active">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html" class="fa-university">About Us</a>
+                                  <ul>
+                                        <li>
+                                            <a href="about.html"  >Our Chapter</a>
+                                        </li>
+                                        <li>
+                                            <a href="benefits.html">Benefits</a>
+                                        </li>
+                                        <li>
+                                            <a href="requirements.html" >Requirements</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="officers.html" class="fa-user ">Officers</a>
+                                   <ul>
+                                        <li>
+                                            <a href="officers.html"  >Current Officers</a>
+                                        </li>
+                                        <li>
+                                            <a href="descriptions.html" >Position Descriptions</a>
+                                        </li>
+                                       
+                                    </ul>
+                                </li>
+                                
+                                
+                                 <li>
+                                    <a href="https://calendar.google.com/calendar/embed?src=tbpsjsu.org_p2qdu6i9ogbd4fmvcpp4cf5vlc%40group.calendar.google.com&ctz=America/Los_Angeles" target = "_blank" class="fa-calendar">Calendar</a>
+                                </li>
+                                
+                                <li>
+                                    <span class="fa-paper-plane-o ">Links</span>
+                                    <ul>
+                                       
+                                        <li>
+                                            <a href="https://drive.google.com/drive/folders/0BxtUAok7tEutRTVOazBsV3VjM0U" target="_blank">Candidates</a>
+                                        </li>
+                                         <li>
+                                            <a href="mentoring.html" >Mentoring Program</a>
+                                        </li>
+                                                                                
+                                    </ul>
+                                </li>
+                                <li>
+                                    
+                                </li>
+                                
+                                <li>
+                                    <a href="contact.html" class="fa-comment ">Contact</a>
+                                    <ul><li>
+                                            <a href="contact.html" >Contact Us</a>
+                                        </li>
+                                        <li>
+                                            <a href="faq.html">FAQ</a>
+                                        </li> </ul>
+                                </li>
+
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Top Menu -->';
+                ?>
+
+<!-- <?php
+//include $_SERVER['DOCUMENT_ROOT']."/lib/sample.lib.php";
+//http://php.net/manual/en/function.include.php
+?> -->
