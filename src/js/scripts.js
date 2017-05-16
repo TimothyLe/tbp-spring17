5ab4ca89d6e1903061aad9ffa671f390faecfac9
timothyle <timothyle85@gmail.com>
2017-05-16 14:26:03 -0700

    Changing HTML files

---
+++ src/js/scripts.js
@@ -0,0 +1,161 @@
+
+/* Portfolio */
+$(window).load(function() {
+    var $cont = $('.portfolio-group');
+    
+    $cont.isotope({
+        itemSelector: '.portfolio-group .portfolio-item',
+        masonry: {columnWidth: $('.isotope-item:first').width(), gutterWidth: -20, isFitWidth: true},
+        filter: '*',
+    });
+
+    $('.portfolio-filter-container a').click(function() {
+        $cont.isotope({
+            filter: this.getAttribute('data-filter')
+        });
+
+        return false;
+    });
+
+    var lastClickFilter = null;
+    $('.portfolio-filter a').click(function() {
+
+        //first clicked we don't know which element is selected last time
+        if (lastClickFilter === null) {
+            $('.portfolio-filter a').removeClass('portfolio-selected');
+        }
+        else {
+            $(lastClickFilter).removeClass('portfolio-selected');
+        }
+
+        lastClickFilter = this;
+        $(this).addClass('portfolio-selected');
+    });
+
+});
+
+/* Image Hover  - Add hover class on hover */
+$(document).ready(function(){
+    if (Modernizr.touch) {
+        // show the close overlay button
+        $(".close-overlay").removeClass("hidden");
+        // handle the adding of hover class when clicked
+        $(".image-hover figure").click(function(e){
+            if (!$(this).hasClass("hover")) {
+                $(this).addClass("hover");
+            }
+        });
+        // handle the closing of the overlay
+        $(".close-overlay").click(function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            if ($(this).closest(".image-hover figure").hasClass("hover")) {
+                $(this).closest(".image-hover figure").removeClass("hover");
+            }
+        });
+    } else {
+        // handle the mouseenter functionality
+        $(".image-hover figure").mouseenter(function(){
+            $(this).addClass("hover");
+        })
+        // handle the mouseleave functionality
+        .mouseleave(function(){
+            $(this).removeClass("hover");
+        });
+    }
+});
+
+// thumbs animations
+$(function () {
+    
+    $(".thumbs-gallery i").animate({
+             opacity: 0
+    
+          }, {
+             duration: 300,
+             queue: false
+          });
+
+   $(".thumbs-gallery").parent().hover(
+       function () {},
+       function () {
+          $(".thumbs-gallery i").animate({
+             opacity: 0
+          }, {
+             duration: 300,
+             queue: false
+          });
+   });
+ 
+   $(".thumbs-gallery i").hover(
+      function () {
+          $(this).animate({
+             opacity: 0
+    
+          }, {
+             duration: 300,
+             queue: false
+          });
+
+          $(".thumbs-gallery i").not( $(this) ).animate({
+             opacity: 0.4         }, {
+             duration: 300,
+             queue: false
+          });
+      }, function () {
+      }
+   );
+
+});
+
+// Mobile Menu
+$(function(){
+    $('#hornavmenu').slicknav();
+    $( "div.slicknav_menu" ).addClass( "hidden-lg" );
+});
+
+// Stellar Parallax
+$(function(){
+  if (Modernizr.touch) {
+      } else {
+          $(window).stellar({
+      horizontalScrolling: false
+    });
+  }
+});
+
+//Target Blank replacement
+$(function() { 
+  $('a[href^=http]').click( function() {  
+    window.open(this.href);
+    return false;
+  });
+});
+
+// Chrome desktop notifications
+document.addEventListener('DOMContentLoaded', function () {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Tau Beta Pi California Eta', {
+      icon: "https://tbp.berkeley.edu/static/images/bent_circle.png",
+      body: "Greetings from the bent!",
+    });
+
+    notification.onclick = function () {
+      window.open("http://stackoverflow.com/a/13328397/1269037");      
+    };
+
+  }
+
+}
